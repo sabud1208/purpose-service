3837cd8fd35b15ee060ba27385ec290a6e3bf06c
Stephanie <sabud1208@gmail.com>
2023-12-16 13:18:02 -0500

    update age pref method

--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -1,7 +1,7 @@
 //importing modules
 const express = require('express')
 const userController = require('../Controllers/userController')
-const { signup, login, authenticateToken, getAllUsers, getUser, getAgePreference } = userController
+const { signup, login, authenticateToken, getAllUsers, getUser, getAgePreference, updateUserAgePreferences } = userController
 const userAuth = require('../Middlewares/userAuth')
 const app = express();
 const router = express.Router()
@@ -12,6 +12,7 @@ router.post('/signup', userAuth.saveUser, signup)
 //login route
 router.post('/login', login )
 
+router.post('/updateAgePreference', updateUserAgePreferences)
 //auth route
 router.get('/auth', authenticateToken)
 
@@ -28,6 +29,19 @@ router.get('/getAllUsers', async (req, res) => {
     };
 })
 
+router.get('/getId', async (req, res) =>{
+  try {
+    if (req.isAuthenticated()) {
+      const currentUserId = req.user.id;
+      res.send(`Current user ID: ${currentUserId}`);
+    } else {
+      res.redirect('/login');
+    }
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+})
 router.get('/getUser', getUser)
 
 router.post('/agePreference', getAgePreference)
